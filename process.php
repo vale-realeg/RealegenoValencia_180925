fcd4755e9a5bb0ac49337d0118e7c869a7aa6d80
Valeria Realegeno <ia.valeria_realegeno@ufg.edu.sv>
2025-09-25 19:49:56 -0600

    practica php finalizada

--- process.php
+++ process.php
@@ -2,7 +2,27 @@
 require("classes/estudiante.class.php");
 $Estudiantes = new Estudiante();
 
-$resultado = $Estudiantes->obtenerEstudiantes();
+//VARIABLE ESTUDIANTE
+$resultado = [];
+
+if($_SERVER["REQUEST_METHOD"]=="GET"){
+    $tipo_peticion = ((isset($_GET["t"])) ?($_GET["t"]) : null);
+
+    if($tipo_peticion=="selectAll"){
+        $resultado = $Estudiantes->obtenerEstudiantes();
+    }else{
+        $resultado = [];
+    }
+}elseif ($_SERVER["REQUEST_METHOD"]=="POST") {
+    if(isset($_POST["fecha_nac"]) and isset($_POST["id_genero"])){
+        $resultado = $Estudiantes->nuevoEstudiante($_POST["fecha_nac"],$_POST["id_genero"]);
+    }else{
+        header('HTTP/1.1 400 Bad Request');
+        $resultado = array("mensaje"=>"Parametros no Enviados");
+    }
+}
+
+//$resultado = $Estudiantes->obtenerEstudiantes();
 
 header("Content-Type: Application/json");
 echo(json_encode($resultado));
